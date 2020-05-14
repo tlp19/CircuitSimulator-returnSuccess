92afe36ada52daecf07bae89d168379614d3c6ec
tlp19 <tlp19@imperial.ac.uk>
2020-05-14 16:04:48 +0100

    Basic files created

---
+++ header.hpp
@@ -0,0 +1,10 @@
+#ifndef header_hpp
+#define header_hpp
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cassert>
+
+#endif
---
+++ read_input.cpp
@@ -0,0 +1,10 @@
+#include "header.hpp"
+
+using namespace std;
+
+int main()
+{
+
+
+
+}
