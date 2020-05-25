c4e45f04f28b1a71f2b4e3160f0044c646435234
bd519 <bd519@imperial.ac.uk>
2020-05-25 16:34:39 +0100

    revised version of the test output component

--- test_output_component.cpp
+++ test_output_component.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include "network_functions.cpp"
+
 
 int main(){
 
@@ -14,26 +14,24 @@ x.instruction.timestep = "0.2";
 Component resistor;
 resistor.type = 'R';
 resistor.name = "1";
+resistor.nb_branches = 2;
 resistor.nodes.push_back("0");
 resistor.nodes.push_back("N1");
-resistor.value = 12;
+resistor.value = "12";
 
 c.push_back(resistor);
 
 Component cap;
 cap.type = 'C';
 cap.name = "1";
+cap.nb_branches = 2;
 cap.nodes.push_back("N1");
 cap.nodes.push_back("N2");
-cap.value = 0.1;
+cap.value = "0.1";
 
 c.push_back(cap);
+
 x.components = c;
 cout<< x<< endl;
-
-
-
-
-
 
 }
