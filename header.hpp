835b24e1eb5fb1453ebb77cb70f360237f41620a
tlp19 <tlp19@imperial.ac.uk>
2020-05-25 10:41:40 +0100

    started coding header

--- header.hpp
+++ header.hpp
@@ -7,4 +7,26 @@
 #include <string>
 #include <cassert>
 
+using namespace std;
+
+struct Component {
+	char type;
+	string name;
+	int nb_branches;
+	vector<string> nodes;
+	float value;
+	string transistor_type;
+
+}
+
+struct
+
+class Network {
+	vector<Component> components;
+	vector<Instruction> instructions;
+}
+
+int find_nb_branches(Component input);
+
+
 #endif
