d6d06c893179bec1888cf398bcdc2d51a73cd925
bd519 <bd519@imperial.ac.uk>
2020-05-28 15:32:59 +0100

    formating

--- network_header.hpp
+++ network_header.hpp
@@ -25,18 +25,18 @@ struct Sine_function {
 // Struct to define a specific component inside the circuit
 class Component {
   public:
-	char type;						//Type of the component; either V, I, R, C, L, D or Q
-	string name;					//Arbitrary name of the component
-	int nb_branches;				//Number of nodes that the component is connected to
-	vector<string> nodes;			//List of the nodes that the component is connected to (2 or 3)
-	string value;					//Litteral value of the component (in Ohms, Farads, etc.)
-	double num_value;				//Numerical value extracted from the value variable
-	bool has_function;				//For V and I: 0 if only DC vlaue, 1 if using a sine function
-	Sine_function function;			//Sine function for V and I components
-	string transistor_type;			//For Q: either NPN or PNP
+	char type;						  //Type of the component; either V, I, R, C, L, D or Q
+	string name;					  //Arbitrary name of the component
+	int nb_branches;				  //Number of nodes that the component is connected to
+	vector<string> nodes;			  //List of the nodes that the component is connected to (2 or 3)
+	string value;					  //Litteral value of the component (in Ohms, Farads, etc.)
+	double num_value;				  //Numerical value extracted from the value variable
+	bool has_function;				  //For V and I: 0 if only DC vlaue, 1 if using a sine function
+	Sine_function function;			  //Sine function for V and I components
+	string transistor_type;			  //For Q: either NPN or PNP
 
-	string value_or_type() const;	//Returns the transis. type if Q, returns the value otherwise
-	void set_nb_branches();			//Fills in the nb_branches member variable
+	string value_or_type() const;	  //Returns the transis. type if Q, returns the value otherwise
+	void set_nb_branches();			  //Fills in the nb_branches member variable
 	string function_to_string() const;	//Writes a Sine_function as a string
 };
 
